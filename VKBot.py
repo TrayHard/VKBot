a655bb9754882ef6ae6c853234673f919379218c
TrayHard <wwwtrayfrost@gmail.com>
2018-08-28 19:27:04 +0300

    Data, time, keyboard:

--- VKBot.py
+++ VKBot.py
@@ -7,12 +7,11 @@ import datetime
 vk = vk_api.VkApi(token=data.key)
 
 
-def send_msg(peer_id, text):
-    vk.method("messages.send", {'peer_id': peer_id, 'message': text})
-
-
-def send_keyboard(peer_id, text, keyboard):
-    vk.method("messages.send", {'peer_id': peer_id, 'message': text, 'keyboard': keyboard})
+def send_msg(peer_id, text, keyboard=None):
+    if keyboard is None:
+        vk.method("messages.send", {'peer_id': peer_id, 'message': text})
+    else:
+        vk.method("messages.send", {'peer_id': peer_id, 'message': text, 'keyboard': keyboard})
 
 
 def get_button(label, color, payload=""):
@@ -35,7 +34,6 @@ keyboard = {
         ]
     ]
 }
-
 keyboard = json.dumps(keyboard, ensure_ascii=False).encode('utf-8')
 keyboard = str(keyboard.decode('utf-8'))
 
@@ -46,19 +44,17 @@ while True:
     if messages["count"] >= 1:
         user_id = messages["items"][0]["last_message"]["from_id"]
         body = messages["items"][0]["last_message"]["text"]
-        text = ""
         if body.lower() == "клавиатура":
-            send_keyboard(user_id, "Выберите нужный вариант", keyboard)
+            send_msg(user_id, "Выберите нужный вариант", keyboard)
         elif body.lower() == "время":
             now = datetime.datetime.now()
-            text = "Текущее время: "+now.strftime("%H:%M")
+            send_msg(user_id, "Текущее время: " + now.strftime("%H:%M") + "\nВыберите нужный вариант", keyboard)
         elif body.lower() == "дата":
             now = datetime.datetime.now()
-            text = "Текущая дата: " + now.strftime("%d-%m-%Y")
+            send_msg(user_id, "Текущая дата: " + now.strftime("%d-%m-%Y") + "\nВыберите нужный вариант", keyboard)
         else:
-            text = "Я тебя не понял";
-        send_msg(user_id, text)
-    time.sleep(1)
+            send_msg(user_id, "Я тебя не понял")
+        time.sleep(1)
 
 
 
